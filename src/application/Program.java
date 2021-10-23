f393f7abde3959216ac7e7eaf3bf5105ed42e26c
juniorn06 <juniorn06@gmail.com>
2021-10-23 19:30:06 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,6 +6,7 @@ import entities.Department;
 import entities.Seller;
 
 import java.util.Date;
+import java.util.List;
 
 public class Program {
 
@@ -17,5 +18,11 @@ public class Program {
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
 
+        System.out.println("\n=== Test 2: seller findByDepartment ===");
+        Department department = new Department(2, null);
+        List<Seller> list =  sellerDao.findByDepartment(department);
+        for (Seller obj : list){
+            System.out.println(obj);
+        }
     }
 }
